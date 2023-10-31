bc83bdef17fb3c53f33736b9ecb279826a5ae82c
Code Wizards <codewizards123@gmail.com>
2023-10-31 15:16:38 +0530

    update

---
+++ user-service/src/main/java/com/example/userservice/model/User.java
@@ -0,0 +1,78 @@
+package com.example.userservice.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String email;
+    private String password;
+    private String role;
+    private String userName;
+    private String telephone;
+
+    public User() {
+        super();
+    }
+
+    public User(String email, String password, String role, String userName, String telephone) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.userName = userName;
+        this.telephone = telephone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+}
---
+++ user-service/src/main/java/com/example/userservice/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package com.example.userservice.repository;
+
+public class UserRepository {
+}
