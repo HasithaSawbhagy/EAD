5018f91d3eaffca13d07d63d471b142ec41a4350
VidathAmarasekara <vidathamarasekara99@gmail.com>
2023-11-02 13:53:55 +0530

    Update order controller

--- order-service/src/main/java/com/SpringbootTest/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/SpringbootTest/orderservice/controller/OrderController.java
@@ -1,11 +1,8 @@
 package com.SpringbootTest.orderservice.controller;
 
-
+import com.SpringbootTest.orderservice.dto.OrderRequest;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/order")
@@ -13,7 +10,7 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void createOrder(){
+    public void createOrder(@RequestBody OrderRequest orderRequest){
 
     }
 }
