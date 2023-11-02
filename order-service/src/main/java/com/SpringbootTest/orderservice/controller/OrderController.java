1e8c6998308865f3720cacc4a9d49a0e7fef31d5
VidathAmarasekara <vidathamarasekara99@gmail.com>
2023-11-02 11:03:56 +0530

    Create Order Controller Class

---
+++ order-service/src/main/java/com/SpringbootTest/orderservice/controller/OrderController.java
@@ -0,0 +1,19 @@
+package com.SpringbootTest.orderservice.controller;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/order")
+public class OrderController {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createOrder(){
+
+    }
+}
