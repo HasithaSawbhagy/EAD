780918ccc5b24e5f279fbd88b2e977b1ed3772d1
VidathAmarasekara <vidathamarasekara99@gmail.com>
2023-11-02 10:21:44 +0530

    create a order table

--- order-service/src/main/java/com/SpringbootTest/orderservice/model/order.java
+++ order-service/src/main/java/com/SpringbootTest/orderservice/model/order.java
@@ -1,2 +1,24 @@
-package com.SpringbootTest.orderservice.model;public class order {
+package com.SpringbootTest.orderservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+
+@Document(value = "order")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class order {
+    @Id
+    private String id;
+    private String user_id;
+    private String order_date;
+    private String status;
+    private String delivery_address;
 }
