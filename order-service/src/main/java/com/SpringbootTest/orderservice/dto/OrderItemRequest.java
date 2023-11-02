146043640fb9fe7e337e54013c1570689ff7ec60
ChamudiChanuki <chanukijayasinghe99@gmail.com>
2023-11-02 21:28:40 +0530

    Update DTO

---
+++ order-service/src/main/java/com/SpringbootTest/orderservice/dto/OrderItemRequest.java
@@ -0,0 +1,20 @@
+package com.SpringbootTest.orderservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItemRequest {
+
+    private String order_id;
+    private String product_id;
+    private int quantity;
+    private BigDecimal price;
+}
---
+++ order-service/src/main/java/com/SpringbootTest/orderservice/dto/OrderItemResponse.java
@@ -0,0 +1,19 @@
+package com.SpringbootTest.orderservice.dto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItemResponse {
+    private String id;
+    private String order_id;
+    private String product_id;
+    private int quantity;
+    private BigDecimal price;
+}
