4499cae9b547236bb83467ca0f0f275eb4dcbeb7
VidathAmarasekara <vidathamarasekara99@gmail.com>
2023-11-02 14:36:49 +0530

    Complete the order create logic

--- order-service/src/main/java/com/SpringbootTest/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/SpringbootTest/orderservice/controller/OrderController.java
@@ -1,16 +1,21 @@
 package com.SpringbootTest.orderservice.controller;
 
 import com.SpringbootTest.orderservice.dto.OrderRequest;
+import com.SpringbootTest.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/order")
+@RequiredArgsConstructor
 public class OrderController {
+
+    private final OrderService orderService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createOrder(@RequestBody OrderRequest orderRequest){
-
+        orderService.createOrder(orderRequest);
     }
 }
--- order-service/src/main/java/com/SpringbootTest/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/SpringbootTest/orderservice/service/OrderService.java
@@ -2,11 +2,26 @@ package com.SpringbootTest.orderservice.service;
 
 import com.SpringbootTest.orderservice.dto.OrderRequest;
 import com.SpringbootTest.orderservice.model.Order;
+import com.SpringbootTest.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class OrderService {
-    public void createOrder(OrderRequest orderRequest){
 
+    private final OrderRepository orderRepository;
+    public void createOrder(OrderRequest orderRequest){
+        Order order = Order.builder()
+                .user_id(orderRequest.getUser_id())
+                .order_date(orderRequest.getOrder_date())
+                .status(orderRequest.getStatus())
+                .delivery_address(orderRequest.getDelivery_address())
+                .build();
+
+        orderRepository.save(order);
+        log.info("Order {} is saved", order.getId());
     }
 }
