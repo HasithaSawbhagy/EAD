75d46b7f3e596b734bb6e34f2377d072b5390a74
ChamudiChanuki <chanukijayasinghe99@gmail.com>
2023-11-02 19:03:12 +0530

    Create orderItem model

---
+++ order-service/src/main/java/com/SpringbootTest/orderservice/model/OrderItem.java
@@ -0,0 +1,24 @@
+package com.SpringbootTest.orderservice.model;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+
+@Document(value = "orderitem")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class OrderItem {
+    @Id
+    private String id;
+    private String order_id;
+    private String product_id;
+    private int quantity;
+    private BigDecimal price;
+
+}
