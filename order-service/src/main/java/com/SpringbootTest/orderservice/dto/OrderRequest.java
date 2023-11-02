c965673ba862d1f7fa8ec64f33a51d6316473d43
VidathAmarasekara <vidathamarasekara99@gmail.com>
2023-11-02 13:53:36 +0530

    update order request

--- order-service/src/main/java/com/SpringbootTest/orderservice/dto/OrderRequest.java
+++ order-service/src/main/java/com/SpringbootTest/orderservice/dto/OrderRequest.java
@@ -1,4 +1,13 @@
 package com.SpringbootTest.orderservice.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class OrderRequest {
+    private String user_id;
+    private String order_date;
+    private String status;
+    private String delivery_address;
 }
