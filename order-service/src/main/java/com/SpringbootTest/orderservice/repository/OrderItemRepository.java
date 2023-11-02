457812406e3d8dfcf3f943b6194dc6f0eb5d3b8a
ChamudiChanuki <chanukijayasinghe99@gmail.com>
2023-11-02 19:13:48 +0530

    Create OrderItem Repo

---
+++ order-service/src/main/java/com/SpringbootTest/orderservice/repository/OrderItemRepository.java
@@ -0,0 +1,8 @@
+package com.SpringbootTest.orderservice.repository;
+
+
+import com.SpringbootTest.orderservice.model.OrderItem;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface OrderItemRepository extends MongoRepository<OrderItem, String>{
+}
