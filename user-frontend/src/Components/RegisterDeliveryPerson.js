9a5fa9c4e9648e53a7c45d7ba7906c72df03b71d
NithyaAdhi <113980392+NithyaAdhi@users.noreply.github.com>
2023-11-03 21:57:29 +0530

    update

--- user-frontend/src/Components/RegisterDeliveryPerson.js
+++ user-frontend/src/Components/RegisterDeliveryPerson.js
@@ -3,117 +3,176 @@ import axios from "axios";
 import "../Css/register.css";
 
 function RegisterDeliveryPerson() {
-
   const [email, setEmail] = useState("");
   const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const [role, setRole] = useState("");
   const [telephone, setTelephone] = useState("");
   const [areacode, setAreacode] = useState("");
+  const [errors, setErrors] = useState({});
+
+  const validateEmail = (email) => {
+    // Email validation regex
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const validateTelephone = (telephone) => {
+    // Telephone validation regex (10 digits only)
+    const telephoneRegex = /^\d{10}$/;
+    return telephoneRegex.test(telephone);
+  };
+
+  const validateForm = () => {
+    const errors = {};
+
+    if (!username.trim()) {
+      errors.username = "Username is required";
+    }
+
+    if (!email.trim()) {
+      errors.email = "Email is required";
+    } else if (!validateEmail(email)) {
+      errors.email = "Invalid email format";
+    }
+
+    if (!password.trim()) {
+      errors.password = "Password is required";
+    }
+
+    if (!role.trim()) {
+      errors.role = "Role is required";
+    }
+
+    if (!telephone.trim()) {
+      errors.telephone = "Telephone is required";
+    } else if (!validateTelephone(telephone)) {
+      errors.telephone = "Invalid telephone number (10 digits required)";
+    }
+
+    setErrors(errors);
+    return Object.keys(errors).length === 0; // Return true if there are no errors
+  };
 
   async function save(event) {
     event.preventDefault();
-    try {
-      await axios.post("http://localhost:8080/users/register_DelveryPerson", {
-        email: email,
-        username: username,
-        password: password,
-        role: role,
-        telephone: telephone,
-        areacode: areacode,
-      });
-      alert("User Registation Successfully");
 
-    } catch (err) {
-      alert(err);
+    if (validateForm()) {
+      try {
+        await axios.post("http://localhost:8080/users/register_DelveryPerson", {
+          email: email,
+          username: username,
+          password: password,
+          role: role,
+          telephone: telephone,
+          areacode: areacode,
+        });
+        alert("User Registration Successfully");
+      } catch (err) {
+        alert(err);
+      }
     }
   }
 
   return (
     <div>
-      <div class="container mt-4" >
-        <div class="card">
-          <h1>User Registation</h1>
+      <div className="container mt-4">
+        <div className="card">
+          <h1>User Registration</h1>
 
           <form>
-            <div class="form-group">
+            <div className="form-group">
               <label>User Name</label>
-              <input type="text" class="form-control" id="username" placeholder="Enter Name"
-
+              <input
+                type="text"
+                className="form-control"
+                id="username"
+                placeholder="Enter Name"
                 value={username}
                 onChange={(event) => {
                   setUserName(event.target.value);
                 }}
               />
-
+              {errors.username && <p className="error">{errors.username}</p>}
             </div>
 
-            <div class="form-group">
+            <div className="form-group">
               <label>Email</label>
-              <input type="email" class="form-control" id="email" placeholder="Enter Email"
-
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="Enter Email"
                 value={email}
                 onChange={(event) => {
                   setEmail(event.target.value);
                 }}
-
               />
-
+              {errors.email && <p className="error">{errors.email}</p>}
             </div>
 
-            <div class="form-group">
+            <div className="form-group">
               <label>Password</label>
-              <input type="password" class="form-control" id="password" placeholder="Enter password"
-
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Enter password"
                 value={password}
                 onChange={(event) => {
                   setPassword(event.target.value);
                 }}
-
               />
+              {errors.password && <p className="error">{errors.password}</p>}
             </div>
 
-            <div class="form-group">
+            <div className="form-group">
               <label>Role</label>
-              <input type="role" class="form-control" id="role" placeholder="Enter User Role"
-
+              <input
+                type="text"
+                className="form-control"
+                id="role"
+                placeholder="Enter User Role"
                 value={role}
                 onChange={(event) => {
                   setRole(event.target.value);
                 }}
-
               />
-
+              {errors.role && <p className="error">{errors.role}</p>}
             </div>
 
-            <div class="form-group">
+            <div className="form-group">
               <label>Telephone</label>
-              <input type="telephone" class="form-control" id="telephone" placeholder="Enter Telephone"
-
+              <input
+                type="text"
+                className="form-control"
+                id="telephone"
+                placeholder="Enter Telephone"
                 value={telephone}
                 onChange={(event) => {
                   setTelephone(event.target.value);
                 }}
-
               />
-
+              {errors.telephone && <p className="error">{errors.telephone}</p>}
             </div>
 
-            <div class="form-group">
+            <div className="form-group">
               <label>Area Code</label>
-              <input type="areacode" class="form-control" id="areacode" placeholder="Enter area code"
-
+              <input
+                type="text"
+                className="form-control"
+                id="areacode"
+                placeholder="Enter area code"
                 value={areacode}
                 onChange={(event) => {
                   setAreacode(event.target.value);
                 }}
-
               />
-
             </div>
 
-            <button type="submit" class="btn btn-primary mt-4" onClick={save} >Save</button>
-
+            <button type="submit" className="btn btn-primary mt-4" onClick={save}>
+              Save
+            </button>
           </form>
         </div>
       </div>
--- user-frontend/src/Components/RegisterInventoryKeeper.js
+++ user-frontend/src/Components/RegisterInventoryKeeper.js
@@ -1,109 +1,167 @@
-import {  useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 import "../Css/register.css";
 
 function RegisterInventoryKeeper() {
-  
-    const [email, setEmail] = useState("");
-    const [username, setUserName] = useState("");
-    const [password, setPassword] = useState("");
-    const [role, setRole] = useState("");
-    const [telephone, setTelephone] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("");
+  const [telephone, setTelephone] = useState("");
+  const [errors, setErrors] = useState({});
 
-    async function save(event) {
-        event.preventDefault();
-        try {
-          await axios.post("http://localhost:8080/users/register_InventoryKeeper", {
+  const validateEmail = (email) => {
+    // Email validation regex
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const validateTelephone = (telephone) => {
+    // Telephone validation regex (10 digits only)
+    const telephoneRegex = /^\d{10}$/;
+    return telephoneRegex.test(telephone);
+  };
+
+  const validateForm = () => {
+    const errors = {};
+
+    if (!username.trim()) {
+      errors.username = "Username is required";
+    }
+
+    if (!email.trim()) {
+      errors.email = "Email is required";
+    } else if (!validateEmail(email)) {
+      errors.email = "Invalid email format";
+    }
+
+    if (!password.trim()) {
+      errors.password = "Password is required";
+    }
+
+    if (!role.trim()) {
+      errors.role = "Role is required";
+    }
+
+    if (!telephone.trim()) {
+      errors.telephone = "Telephone is required";
+    } else if (!validateTelephone(telephone)) {
+      errors.telephone = "Invalid telephone number (10 digits required)";
+    }
+
+    setErrors(errors);
+    return Object.keys(errors).length === 0; // Return true if there are no errors
+  };
+
+  async function save(event) {
+    event.preventDefault();
+
+    if (validateForm()) {
+      try {
+        await axios.post("http://localhost:8080/users/register_InventoryKeeper", {
           email: email,
           username: username,
           password: password,
-          role:role,
-          telephone:telephone
-          });
-          alert("User Registation Successfully");
-
-        } catch (err) {
-          alert(err);
-        }
+          role: role,
+          telephone: telephone,
+        });
+        alert("User Registration Successfully");
+      } catch (err) {
+        alert(err);
       }
-  
-    return (
-    <div>
-    <div class="container mt-4" >
-    <div class="card">
-            <h1>User Registation</h1>
-    
-    <form>
-        <div class="form-group">
-          <label>User Name</label>
-          <input type="text"  class="form-control" id="username" placeholder="Enter Name"
-          
-          value={username}
-          onChange={(event) => {
-            setUserName(event.target.value);
-          }}
-          />
-
-        </div>
-
-        <div class="form-group">
-          <label>Email</label>
-          <input type="email"  class="form-control" id="email" placeholder="Enter Email"
-          
-          value={email}
-          onChange={(event) => {
-            setEmail(event.target.value);
-          }}
-          
-          />
- 
-        </div>
-
-        <div class="form-group">
-            <label>Password</label>
-            <input type="password"  class="form-control" id="password" placeholder="Enter password"
-            
-            value={password}
-            onChange={(event) => {
-              setPassword(event.target.value);
-            }}
-            
-            />
-          </div>
-
-          <div class="form-group">
-          <label>Role</label>
-          <input type="role"  class="form-control" id="role" placeholder="Enter User Role"
-          
-          value={role}
-          onChange={(event) => {
-            setRole(event.target.value);
-          }}
-          
-          />
- 
-        </div>
-
-        <div class="form-group">
-          <label>Telephone</label>
-          <input type="telephone"  class="form-control" id="telephone" placeholder="Enter Telephone"
-          
-          value={telephone}
-          onChange={(event) => {
-            setTelephone(event.target.value);
-          }}
-          
-          />
- 
-        </div>
-
-        <button type="submit" class="btn btn-primary mt-4" onClick={save} >Save</button>
-       
-      </form>
-    </div>
-    </div>
-     </div>
-    );
+    }
   }
-  
-  export default RegisterInventoryKeeper;
+
+  return (
+    <div>
+      <div className="container mt-4">
+        <div className="card">
+          <h1>User Registration</h1>
+
+          <form>
+            <div className="form-group">
+              <label>User Name</label>
+              <input
+                type="text"
+                className="form-control"
+                id="username"
+                placeholder="Enter Name"
+                value={username}
+                onChange={(event) => {
+                  setUserName(event.target.value);
+                }}
+              />
+              {errors.username && <p className="error">{errors.username}</p>}
+            </div>
+
+            <div className="form-group">
+              <label>Email</label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="Enter Email"
+                value={email}
+                onChange={(event) => {
+                  setEmail(event.target.value);
+                }}
+              />
+              {errors.email && <p className="error">{errors.email}</p>}
+            </div>
+
+            <div className="form-group">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Enter password"
+                value={password}
+                onChange={(event) => {
+                  setPassword(event.target.value);
+                }}
+              />
+              {errors.password && <p className="error">{errors.password}</p>}
+            </div>
+
+            <div className="form-group">
+              <label>Role</label>
+              <input
+                type="text"
+                className="form-control"
+                id="role"
+                placeholder="Enter User Role"
+                value={role}
+                onChange={(event) => {
+                  setRole(event.target.value);
+                }}
+              />
+              {errors.role && <p className="error">{errors.role}</p>}
+            </div>
+
+            <div className="form-group">
+              <label>Telephone</label>
+              <input
+                type="text"
+                className="form-control"
+                id="telephone"
+                placeholder="Enter Telephone"
+                value={telephone}
+                onChange={(event) => {
+                  setTelephone(event.target.value);
+                }}
+              />
+              {errors.telephone && <p className="error">{errors.telephone}</p>}
+            </div>
+
+            <button type="submit" className="btn btn-primary mt-4" onClick={save}>
+              Save
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default RegisterInventoryKeeper;
