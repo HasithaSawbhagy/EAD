f16f47fbb410a248867841687c30bd1622397ef1
Nethmi-24 <batawalann.20@uom.lk>
2023-11-05 12:57:00 +0530

    Second commit

--- delivery-service/src/main/java/com/springbootExample1/productservice/controller/DeliveryContoller.java
+++ delivery-service/src/main/java/com/springbootExample1/productservice/controller/DeliveryContoller.java
@@ -38,6 +38,14 @@ deliveryService.createDelivery(deliveryRequest);
     public void completeDelivery(@PathVariable String id) {
         deliveryService.completeDelivery(id);
     }
+
+    @PutMapping("/{id}/assignDeliveryPerson")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateDeliveryPersonInfo(@PathVariable String id, @RequestParam String delPersonId,
+            @RequestParam String delPersonName) {
+        deliveryService.updateDeliveryPersonInfo(id, delPersonId, delPersonName);
+    }
+
 }
 
 
@@ -45,39 +53,3 @@ deliveryService.createDelivery(deliveryRequest);
 
 
 
-
-// package com.springbootExample1.productservice.controller;
-
-// import com.springbootExample1.productservice.dto.DeliveryRequest;
-// import com.springbootExample1.productservice.dto.DeliveryResponse;
-// import com.springbootExample1.productservice.service.DeliveryService;
-// import lombok.RequiredArgsConstructor;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.web.bind.annotation.*;
-
-// import java.util.List;
-
-// @RestController
-// @RequestMapping("/api/delivery") 
-// @RequiredArgsConstructor
-// public class DeliveryContoller {
-//     private final DeliveryService deliveryService;
-//     @PostMapping
-//     @ResponseStatus(HttpStatus.CREATED)
-
-//     public void createDelivery(@RequestBody DeliveryRequest deliveryRequest){
-// deliveryService.createDelivery(deliveryRequest);
-
-//     }
-//     @GetMapping
-//     @ResponseStatus(HttpStatus.OK)
-//     public List<DeliveryResponse>getAllDelivery(){
-//         return deliveryService.getAllDelivery();
-//     }
-
-// 	public DeliveryService getDeliveryService() {
-// 		return deliveryService;
-// 	}
-
-
-// }
--- delivery-service/src/main/java/com/springbootExample1/productservice/service/DeliveryService.java
+++ delivery-service/src/main/java/com/springbootExample1/productservice/service/DeliveryService.java
@@ -6,7 +6,13 @@ import com.springbootExample1.productservice.model.Delivery;
 import com.springbootExample1.productservice.repository.DeliveryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
 
@@ -58,85 +64,17 @@ public class DeliveryService {
         deliveryRepository.save(delivery);
         log.info("Delivery {} is completed.", id);
     }
+    public void updateDeliveryPersonInfo(String id, String delPersonId, String delPersonName) {
+        Delivery delivery = deliveryRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Delivery not found"));
+        delivery.setDelPerson_id(delPersonId);
+        delivery.setDelPerson_name(delPersonName);
+        deliveryRepository.save(delivery);
+        log.info("Delivery {} updated with delivery person info: ID={}, Name={}", id, delPersonId, delPersonName);
+    }
 }
 
 
-
-// package com.springbootExample1.productservice.service;
-
-// import com.springbootExample1.productservice.dto.DeliveryRequest;
-// import com.springbootExample1.productservice.dto.DeliveryResponse;
-// import com.springbootExample1.productservice.model.Delivery;
-// import com.springbootExample1.productservice.repository.DeliveryRepository;
-// import lombok.RequiredArgsConstructor;
-// import lombok.extern.slf4j.Slf4j;
-// import org.springframework.stereotype.Service;
-
-// import java.util.List;
-// import java.util.Optional;
-
-// import javax.management.RuntimeErrorException;
-
-// @Service
-// @RequiredArgsConstructor
-// @Slf4j
-// public class DeliveryService {
-
-//     private final DeliveryRepository deliveryRepository;
-
-//     public void createDelivery(DeliveryRequest deliveryRequest){
-//         try{
-//             Delivery delivery= Delivery.builder()
-//                 .order_id(deliveryRequest.getOrder_id())
-//                 .delPerson_id(deliveryRequest.getDelPerson_id())
-//                 .delPerson_name(deliveryRequest.getDelPerson_name())
-//                 // .status(deliveryRequest.getStatus())
-//                 .build();
-//         deliveryRepository.save(delivery);
-//         log.info("Delivery {} is saved",delivery.getId());
-
-//     } catch(Exception e){
-//         log.error("Failed to create delivery: "+ e.getMessage());
-//         throw new RuntimeErrorException(null, "Failed to create order.");
-//     }
-
-//     }
-//     public List<DeliveryResponse> getAllDelivery() {
-//         List<Delivery> deliveries=deliveryRepository.findAll();
-//         return deliveries.stream().map(this::mapToDeliveryResponse).toList();
-//     }
-//     // get by delivery id
-//     // public List<DeliveryResponse> getDeliveryById(String id){
-//     //     List<Delivery> deliveryById=deliveryRepository.findById(id).get();
-//     //     return deliveryById.stream().map(this::mapToDeliveryResponse).toList();
-//     // }
-//     private DeliveryResponse mapToDeliveryResponse (Delivery delivery){
-//         return DeliveryResponse.builder()
-//                 .id(delivery.getId())
-//                 .order_id(delivery.getOrder_id())
-//                 .delPerson_id(delivery.getDelPerson_id())
-//                 .delPerson_name(delivery.getDelPerson_name())
-//                 .status("pending") 
-//                 .build();
-//     }
-
-
-//     public void updateDeliveryStatus(String deliveryId, String newStatus) {
-//     Optional<Delivery> optionalDelivery = deliveryRepository.findById(deliveryId);
-//     if (optionalDelivery.isPresent()) {
-//         Delivery delivery = optionalDelivery.get();
-//         // delivery.setStatus(newStatus);
-//         deliveryRepository.save(delivery);
-//         log.info("Delivery {} status updated to: {}", delivery.getId(), newStatus);
-//     } else {
-//         log.error("Delivery with ID {} not found.", deliveryId);
-//         throw new RuntimeException("Delivery not found.");
-//     }
-//     }
-//     public List<DeliveryResponse> ok(String string) {
-//         return null;
-//     }
-// }
 
 
   
