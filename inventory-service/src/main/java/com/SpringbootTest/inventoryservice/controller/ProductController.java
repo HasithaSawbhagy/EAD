d1cee82ad9140757b08c156932e919eadf2c1a18
Hasitha  Saubhagya <dassanayakahdhs.20@uom.lk>
2023-11-10 18:09:31 +0530

    Bug fixed

--- inventory-service/src/main/java/com/SpringbootTest/inventoryservice/controller/ProductController.java
+++ inventory-service/src/main/java/com/SpringbootTest/inventoryservice/controller/ProductController.java
@@ -29,7 +29,7 @@ public class ProductController {
         productService.createProduct(productRequest);
     }
 
-//get current product quantity by id
+    //get current product quantity by id
     @GetMapping("getQuantityId/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Integer> getProductQuantityById(@PathVariable String id) {
@@ -41,14 +41,12 @@ public class ProductController {
     //find whether the product is in stock or not
     @GetMapping("isInStock")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<Boolean> isProductQuantityGreaterThanZero(@RequestParam String id) {
+    public ResponseEntity<Boolean> isProductQuantityGreaterThanZero(@RequestParam String id,@RequestParam int quantity) {
         Product product = productRepository.findById(id).orElseThrow(() -> new NotFoundException("Product is not found with id:" + id));
-        int quantity = Integer.parseInt(product.getQuantity());
-        boolean isQuantityGreaterThanZero = quantity > 0;
+        int stockQuantity = Integer.parseInt(product.getQuantity());
+        boolean isQuantityGreaterThanZero = stockQuantity > 0 && stockQuantity>=quantity;
         return ResponseEntity.ok(isQuantityGreaterThanZero);
     }
-
-
 
 
     //get all products
@@ -60,15 +58,15 @@ public class ProductController {
 
     @GetMapping("{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Product getProductById(@PathVariable String id){
-        return productRepository.findById(id).orElseThrow(()->new NotFoundException("Product is not found with id:"+id));
+    public Product getProductById(@PathVariable String id) {
+        return productRepository.findById(id).orElseThrow(() -> new NotFoundException("Product is not found with id:" + id));
     }
 
     //delete Product
     @DeleteMapping("delete/{id}")
     public String deleteProduct(@PathVariable String id) {
         if (!productRepository.existsById(id)) {
-            throw new NotFoundException(("Product not found with id" + id)); 
+            throw new NotFoundException(("Product not found with id" + id));
         }
         productRepository.deleteById(id);
         return "Product with id " + id + "has been deleted";
@@ -76,15 +74,15 @@ public class ProductController {
 
     //Update Product
     @PutMapping("update/{id}")
-    public ResponseEntity<Product> updateProduct(@PathVariable String id, @RequestBody Product product){
-        Product updateProduct = productService.updateProduct(id,product);
+    public ResponseEntity<Product> updateProduct(@PathVariable String id, @RequestBody Product product) {
+        Product updateProduct = productService.updateProduct(id, product);
         return ResponseEntity.ok(updateProduct);
     }
 
     //Update  Quantity after purchasing
     @PutMapping("updateQuantity/{id}")
-    public ResponseEntity<Product> updateProductQuantity(@PathVariable String id, @RequestBody Product product){
-        Product updateProductQuantity = productService.updateProductQuantity(id,product);
+    public ResponseEntity<Product> updateProductQuantity(@PathVariable String id, @RequestBody Product product) {
+        Product updateProductQuantity = productService.updateProductQuantity(id, product);
         return ResponseEntity.ok(updateProductQuantity);
     }
 
